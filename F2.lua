220d8fdc41c9484498a7e115921a5dc19b531354
pondduck-beep <pond000duck@gmail.com>
2026-03-19 22:36:54 +0700

    Update F2.lua

--- F2.lua
+++ F2.lua
@@ -1,4 +1,6 @@
-repeat task.wait(2) until game:IsLoaded()
+-- ของฟรียังอยากได้อีกมักมากนะมึง
+
+repeat task.wait(0.5) until game:IsLoaded()
 
 -- =========================
 -- Inventory Log
@@ -104,6 +106,7 @@ task.spawn(function()
 
 end)
 
+
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local VirtualInputManager = game:GetService("VirtualInputManager")
@@ -137,6 +140,7 @@ task.spawn(function()
 
 end)
 
+
 	-- ปิด setting ลดแลค
 	local SettingsToggle = ReplicatedStorage:WaitForChild("RemoteEvents"):WaitForChild("SettingsToggle")
 
@@ -144,6 +148,7 @@ end)
 	"DisablePvP",
 	"DisableVFX",
 	"DisableOtherVFX",
+	"DisableCutscene",
 	"RemoveTexture",
 	"RemoveShadows"
 	}
@@ -179,17 +184,17 @@ task.spawn(function()
 			continue
 		end
 
-		local weapon =
+		local darkBlade =
 			backpack:FindFirstChild("Strongest In History") or
 			char:FindFirstChild("Strongest In History")
 
-	
-		if weapon then
-			if weapon.Parent ~= char then
-				hum:EquipTool(weapon)
+
+		if darkBlade then
+			if darkBlade.Parent ~= char then
+				hum:EquipTool(darkBlade)
 			end
 
-
+		
 		else
 			local args = {"Equip","Strongest In History"}
 
@@ -204,14 +209,16 @@ task.spawn(function()
 end)
 
 
-
 task.spawn(function()
 	while true do
-		task.wait(0.3)
+		task.wait(0.5)
 		VirtualInputManager:SendKeyEvent(true,"X",false,game)
 		task.wait()
 		VirtualInputManager:SendKeyEvent(false,"X",false,game)
+
+	
 	end
+
 end)
 
 
@@ -231,40 +238,86 @@ local function portal(name)
 end
 
 
+-- ======================
+-- FARM NPC UNTIL DEAD SYSTEM
+-- ======================
+
+local function isAlive(npc)
+	if npc and npc:FindFirstChild("Humanoid") then
+		return npc.Humanoid.Health > 0
+	end
+	return false
+end
+
+local function farmGroup(prefix)
+	local npcsFolder = workspace:WaitForChild("NPCs")
+
+	while true do
+		local allDead = true
+
+		for i = 1,5 do
+			local npc = npcsFolder:FindFirstChild(prefix..i)
+
+			if npc and isAlive(npc) then
+				allDead = false
+
+				-- วาร์ปไปตี
+				if npc:FindFirstChild("HumanoidRootPart") then
+					fasttp(npc.HumanoidRootPart.CFrame * CFrame.new(0,0,3))
+				end
+
+				task.wait(0.2)
+			end
+		end
+
+		if allDead then
+			break
+		end
+
+		task.wait(0.2)
+	end
+end
+
+-- ======================
+-- LOOP FARM ALL MAP
+-- ======================
+
 while true do
 
-
+	-- Shibuya
 	portal("Shibuya")
-	task.wait(0.3)
-	fasttp(CFrame.new(1400.0594482421875,8.486136436462402,484.9847106933594))
-	task.wait(2)
+	task.wait(1)
+	farmGroup("Sorcerer")
 
+	-- HuecoMundo
 	portal("HuecoMundo")
-	task.wait(0.3)
-	fasttp(CFrame.new(-369.4567565917969,-0.15934494137763977,1092.5155029296875))
-	task.wait(2)
+	task.wait(1)
+	farmGroup("Hollow")
 
+	-- Shinjuku (รอบแรก)
 	portal("Shinjuku")
-	task.wait(0.3)
-	fasttp(CFrame.new(-17.37151527404785,1.8984041213989258,-1842.671630859375))
-	task.wait(2)
+	task.wait(1)
+	farmGroup("Curse")
 
-	fasttp(CFrame.new(666.2935180664062,1.8831005096435547,-1692.1214599609375))
-	task.wait(2)
+	-- Shinjuku (รอบสอง)
+	portal("Shinjuku")
+	task.wait(1)
+	farmGroup("StrongSorcerer")
 
+	-- Slime
 	portal("Slime")
-	task.wait(0.3)
-	fasttp(CFrame.new(-1123.855224609375,13.91822624206543,368.31768798828125))
-	task.wait(2)
+	task.wait(1)
+	farmGroup("Slime")
 
+	-- Academy
 	portal("Academy")
-	task.wait(0.3)
-	fasttp(CFrame.new(1068.37646484375,1.778355360031128,1277.8568115234375))
-	task.wait(2)
+	task.wait(1)
+	farmGroup("AcademyTeacher")
 
+	-- Judgement
 	portal("Judgement")
-	task.wait(0.3)
-	fasttp(CFrame.new(-1270.6287841796875,1.177457332611084,-1192.44189453125))
-	task.wait(2)
+	task.wait(1)
+	farmGroup("Swordsman")
 
+	-- วนใหม่
 end
