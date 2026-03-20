2766b61cd58480ad06ee356918d3aafb58c4c905
pondduck-beep <pond000duck@gmail.com>
2026-03-20 12:35:46 +0700

    Update F2.lua

--- F2.lua
+++ F2.lua
@@ -1,6 +1,6 @@
 -- ของฟรียังอยากได้อีกมักมากนะมึง
 
-repeat task.wait(2) until game:IsLoaded()
+repeat task.wait(0.5) until game:IsLoaded()
 
 -- =========================
 -- Inventory Log
@@ -148,7 +148,6 @@ end)
 	"DisablePvP",
 	"DisableVFX",
 	"DisableOtherVFX",
-	"DisableCutscene",
 	"RemoveTexture",
 	"RemoveShadows"
 	}
@@ -215,7 +214,10 @@ task.spawn(function()
 		VirtualInputManager:SendKeyEvent(true,"X",false,game)
 		task.wait()
 		VirtualInputManager:SendKeyEvent(false,"X",false,game)
-
+		task.wait(0.5)
+		VirtualInputManager:SendKeyEvent(true,"V",false,game)
+		task.wait()
+		VirtualInputManager:SendKeyEvent(false,"V",false,game)
 	
 	end
 
@@ -238,86 +240,41 @@ local function portal(name)
 end
 
 
--- ======================
--- FARM NPC UNTIL DEAD SYSTEM
--- ======================
-
-local function isAlive(npc)
-	if npc and npc:FindFirstChild("Humanoid") then
-		return npc.Humanoid.Health > 0
-	end
-	return false
-end
-
-local function farmGroup(prefix)
-	local npcsFolder = workspace:WaitForChild("NPCs")
-
-	while true do
-		local allDead = true
-
-		for i = 1,5 do
-			local npc = npcsFolder:FindFirstChild(prefix..i)
-
-			if npc and isAlive(npc) then
-				allDead = false
-
-				-- วาร์ปไปตี
-				if npc:FindFirstChild("HumanoidRootPart") then
-					fasttp(npc.HumanoidRootPart.CFrame * CFrame.new(0,0,3))
-				end
-
-				task.wait(0.2)
-			end
-		end
-
-		if allDead then
-			break
-		end
-
-		task.wait(0.2)
-	end
-end
-
--- ======================
--- LOOP FARM ALL MAP
--- ======================
-
 while true do
 
-	-- Shibuya
 	portal("Shibuya")
-	task.wait(1)
-	farmGroup("Sorcerer")
+	task.wait(0.5)
+	fasttp(CFrame.new(1400.0594482421875,8.486136436462402,484.9847106933594))
+	task.wait(0.5)
 
-	-- HuecoMundo
 	portal("HuecoMundo")
-	task.wait(1)
-	farmGroup("Hollow")
+	task.wait(0.5)
+	fasttp(CFrame.new(-369.4567565917969,-0.15934494137763977,1092.5155029296875))
+	task.wait(0.5)
 
-	-- Shinjuku (รอบแรก)
 	portal("Shinjuku")
-	task.wait(1)
-	farmGroup("Curse")
+	task.wait(0.5)
+	fasttp(CFrame.new(-17.37151527404785,1.8984041213989258,-1842.671630859375))
+	task.wait(0.5)
 
-	-- Shinjuku (รอบสอง)
-	portal("Shinjuku")
-	task.wait(1)
-	farmGroup("StrongSorcerer")
+	fasttp(CFrame.new(666.2935180664062,1.8831005096435547,-1692.1214599609375))
+	task.wait(0.5)
 
-	-- Slime
 	portal("Slime")
-	task.wait(1)
-	farmGroup("Slime")
+	task.wait(0.5)
+	fasttp(CFrame.new(-1123.855224609375,13.91822624206543,368.31768798828125))
+	task.wait(0.5)
 
-	-- Academy
 	portal("Academy")
-	task.wait(1)
-	farmGroup("AcademyTeacher")
+	task.wait(0.5)
+	fasttp(CFrame.new(1068.37646484375,1.778355360031128,1277.8568115234375))
+	task.wait(0.5)
 
-	-- Judgement
 	portal("Judgement")
-	task.wait(1)
-	farmGroup("Swordsman")
+	task.wait(0.5)
+	fasttp(CFrame.new(-1270.6287841796875,1.177457332611084,-1192.44189453125))
+	task.wait(0.5)
 
-	-- วนใหม่
+
+
 end
